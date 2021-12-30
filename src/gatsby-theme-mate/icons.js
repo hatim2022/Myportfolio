d0f05eb2bb9a652f6990858845507536d24ed1f7
hatim <hatim.joubair@gmail.com>
2021-12-30 14:15:14 -0500

    changes

---
+++ src/gatsby-theme-mate/icons.js
@@ -0,0 +1,19 @@
+import {
+    faMedium,
+    faFacebook,
+    faGithub,
+  } from '@fortawesome/free-brands-svg-icons';
+  import {
+    faGlobe,
+    faEnvelope,
+    faCoffee,
+  } from '@fortawesome/free-solid-svg-icons';
+  
+  export const ICONS = [
+    faMedium,
+    faFacebook,
+    faGithub,
+    faGlobe,
+    faEnvelope,
+    faCoffee,
+  ];
