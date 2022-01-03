42a95e7a110d5bb5bf27566397391af4925e4d07
hatim <hatim.joubair@gmail.com>
2022-01-03 12:47:13 -0500

    add github

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,4 +1,7 @@
-const { ACCESS_TOKEN, SPACE_ID, ANALYTICS_ID } = process.env;
+var { ACCESS_TOKEN, SPACE_ID, ANALYTICS_ID } = process.env;
+SPACE_ID="btjbqxtqwe28";
+ACCESS_TOKEN="v9bZ0NSIxD-ejOMmKkcmU5nRVFsys7UXRSW5N_LHXF4";
+
 
 const plugins = [
   {
